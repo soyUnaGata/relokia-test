f71a9770805daa3544d4c558578a90630a010bc2
soyUnaGata <fallen.snitch@gmail.com>
2024-04-22 21:27:23 +0300

    implement tickets class

---
+++ app/classes/Ticket.php
@@ -0,0 +1,27 @@
+<?php
+
+echo "Hello, World!";
+
+class Ticket
+{
+    public $id;
+    public $description;
+    public $status;
+    public $priority;
+    public $agent_id;
+    public $agent_name;
+    public $agent_email;
+    public $contact_id;
+    public $contact_name;
+    public $contact_email;
+    public $group_id;
+    public $group_name;
+    public $company_id;
+    public $company_name;
+    public $comments = [];
+
+    public function addComment($comment)
+    {
+        $this->comments[] = $comment;
+    }
+}
